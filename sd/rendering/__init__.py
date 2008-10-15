2130b095bafa6ccbb77d8abd18ca39c3a78396d8
trollfot <trollfot@82af7df8-bc4b-4ebc-8022-2999806f7efb>
2008-10-15 20:15:42 +0000

    corrected namespace
    git-svn-id: svn+ssh://trollfot.org/svn/trollfot/projects/sd.rendering/trunk@247 82af7df8-bc4b-4ebc-8022-2999806f7efb

--- sd/rendering/__init__.py
+++ sd/rendering/__init__.py
@@ -1,7 +1,2 @@
-# SD rendering module
+# SD rendering module - an extension to browser views.
 
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
