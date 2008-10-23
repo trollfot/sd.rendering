765eadc08e9cc3430192666d55859f3ea5f9b5dc
trollfot <trollfot@82af7df8-bc4b-4ebc-8022-2999806f7efb>
2008-10-23 19:09:57 +0000

    Grokked the vocabulary
    git-svn-id: svn+ssh://trollfot.org/svn/trollfot/projects/sd.rendering/branches/sd.rendering-simplification@320 82af7df8-bc4b-4ebc-8022-2999806f7efb

--- sd/rendering/vocabularies.py
+++ sd/rendering/vocabularies.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import grokcore.component as grok
 from zope.component import getAdapters
 from zope.i18nmessageid import MessageFactory
 from zope.app.schema.vocabulary import IVocabularyFactory
 from zope.interface.declarations import directlyProvides, implements
+from zope.publisher.browser import TestRequest
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from zope.schema.interfaces import ITokenizedTerm, ITitledTokenizedTerm
-from zope.publisher.browser import TestRequest
-
 from sd.contents.interfaces import IUndirectLayoutProvider
 from interfaces import IStructuredRenderer
 
@@ -20,8 +20,8 @@ class LayoutTerm(object):
     implements(ITokenizedTerm)
     
     def __init__(self, name, docstring):
-        """Create a term from the single value
-        This class prevents the use of the silly bugged SimpleTerm.
+        """Create a term from the single value. This class prevents
+        the use of the silly bugged SimpleTerm.
         """
         self.value = name
         self.token = name
@@ -29,10 +29,11 @@ class LayoutTerm(object):
         directlyProvides(self, ITitledTokenizedTerm)
 
 
-class LayoutVocabulary(object):
+class LayoutVocabulary(grok.GlobalUtility):
     """Vocabulary factory.
     """
-    implements(IVocabularyFactory)
+    grok.name(u"sd.rendering.layout")
+    grok.implements(IVocabularyFactory)
 
     def __call__(self, context):
 
@@ -46,8 +47,3 @@ class LayoutVocabulary(object):
         terms = [LayoutTerm(name, renderer.__doc__)
                  for name, renderer in renderers]
         return SimpleVocabulary(terms)
-
-
-LayoutVocabularyFactory = LayoutVocabulary()
-
-__all__ = ("LayoutVocabularyFactory",)
